539533c5a9e1a8574c16e173ccf924d38c04cd6d
gaelsimon <gsimon@webgeoservices.com>
2015-12-18 11:55:13 +0100

    Update 2015-12-18T10:55:11.569Z

--- js/production.js
+++ js/production.js
@@ -1,1 +1,299 @@
-jQuery(document).ready(function(e){function t(e,t){var s=document.getElementById("zoom-in"),i=document.getElementById("zoom-out");e.appendChild(s),e.appendChild(i),google.maps.event.addDomListener(s,"click",function(){t.setZoom(t.getZoom()+1)}),google.maps.event.addDomListener(i,"click",function(){t.setZoom(t.getZoom()-1)})}function s(e,t){var s=document.getElementById("map-control"),i=document.getElementById("satellite-control");e.appendChild(s),e.appendChild(i),google.maps.event.addDomListener(s,"click",function(){t.setMapTypeId(google.maps.MapTypeId.TERRAIN)}),google.maps.event.addDomListener(i,"click",function(){t.setMapTypeId(google.maps.MapTypeId.SATELLITE)})}var i=44.218895,n=3.676595,o=10,a=navigator.userAgent.toLowerCase().indexOf("trident")>-1,l=a?"img/default_marker.png":"img/default_marker.svg",r="#149DB5",y=-20,u=5,p=[{elementType:"labels",stylers:[{saturation:y}]},{featureType:"poi",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"road.highway",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"road.local",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"road.arterial",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"geometry.stroke",stylers:[{visibility:"off"}]},{featureType:"transit",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"poi",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"poi.government",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"poi.sport_complex",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"poi.attraction",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"poi.business",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"transit",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"transit.station",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"landscape",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"road",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"road.highway",elementType:"geometry.fill",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]},{featureType:"water",elementType:"geometry",stylers:[{hue:r},{visibility:"on"},{lightness:u},{saturation:y}]}],m={center:new google.maps.LatLng(i,n),zoom:o,panControl:!1,zoomControl:!1,mapTypeControl:!1,streetViewControl:!1,mapTypeId:google.maps.MapTypeId.TERRAIN,scrollwheel:!1,styles:p},d=new google.maps.Map(document.getElementById("google-container"),m),g=(new google.maps.Marker({position:new google.maps.LatLng(i,n),map:d,visible:!0,icon:l}),document.createElement("div")),T=document.createElement("div");new t(g,d),new s(T,d);d.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(g),d.controls[google.maps.ControlPosition.LEFT_TOP].push(T);new google.maps.Geocoder});var addressesLocator={stores:[{name:"Truite 1",address:"104 Route de Montfavet 84000 Avignon FRANCE",type:"A"},{name:"Truite 2",address:"5 Avenue Louis Pasteur 84130 Le Pontet FRANCE",type:"A"},{name:"Truite 3",address:"Chemin de la Croix Verte 84000 Avignon FRANCE",type:"A"},{name:"Truite 4",address:"5 Ancien Chemin du Bac de Ramatuel 84000 Avignon FRANCE",type:"A"},{name:"Truite 5",address:"Impasse Villeverte 84000 Avignon FRANCE",type:"A"},{name:"Truite 6",address:"944 Chemin de la Montagne 84700 Sorgues FRANCE",type:"B"},{name:"Truite 7",address:"Boulevard Salvador Allende 84700 Sorgues FRANCE",type:"B"},{name:"Truite 8",address:"30 Rue de la Justice 30133 Les Angles FRANCE",type:"B"},{name:"Truite 9",address:"7 Avenue des Pastorales 30133 Les Angles FRANCE",type:"B"},{name:"Truite 10",address:"5 Chemin du Mervelin 30400 Villeneuve-lès-Avignon FRANCE",type:"C"},{name:"Truite 11",address:"187 Rue Edmond Rostand 84270 Vedène FRANCE",type:"C"},{name:"Truite 12",address:"41 Impasse Daudet 84320 Entraigues-sur-la-Sorgue FRANCE",type:"C"},{name:"Truite 13",address:"215 Avenue Achille Maureau 84700 Sorgues FRANCE",type:"C"},{name:"Truite 14",address:"1410 Chemin des Jonquiers 84210 Pernes-les-Fontaines FRANCE",type:"D"},{name:"Truite 15",address:"D942 84170 Monteux FRANCE",type:"E"},{name:"Truite 16",address:"1054 Avenue Eisenhower 84200 Carpentras FRANCE",type:"E"}]};
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+var AutoComplete = function (map) {
+    var searchInput = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(searchInput);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchInput);
+    this.registerEvent(map, searchBox);
+    return {};
+};
+
+AutoComplete.prototype.registerEvent = function (map, searchBox) {
+    var markers = [];
+    var is_internetExplorer11 = navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+    var marker_url = ( is_internetExplorer11 ) ? 'img/default_marker.png' : 'img/default_marker.svg';
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function () {
+        searchBox.setBounds(map.getBounds());
+    });
+    // [START region_getplaces]
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function (marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: marker_url,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+};
+module.exports = AutoComplete;
+
+},{}],2:[function(require,module,exports){
+var MapStyle = require('./mapstyle');
+var AutoComplete = require('./autocomplete');
+
+window.initMap = function () {
+    var latitude = 44.218895,
+        longitude = 3.676595,
+        map_zoom = 10;
+
+    var style = MapStyle.getStyle();
+    var map_options = {
+        center: new google.maps.LatLng(latitude, longitude),
+        zoom: map_zoom,
+        panControl: false,
+        zoomControl: false,
+        mapTypeControl: false,
+        streetViewControl: false,
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        scrollwheel: false,
+        styles: style
+    };
+    var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+
+    function CustomZoomControl(controlDiv, map) {
+        var controlUIzoomIn = document.getElementById('zoom-in'),
+            controlUIzoomOut = document.getElementById('zoom-out');
+        controlDiv.appendChild(controlUIzoomIn);
+        controlDiv.appendChild(controlUIzoomOut);
+
+        google.maps.event.addDomListener(controlUIzoomIn, 'click', function () {
+            map.setZoom(map.getZoom() + 1)
+        });
+        google.maps.event.addDomListener(controlUIzoomOut, 'click', function () {
+            map.setZoom(map.getZoom() - 1)
+        });
+    }
+
+    function CustomBasemapControl(controlDiv, map) {
+        var controlMap = document.getElementById('map-control'),
+            controlSatellite = document.getElementById('satellite-control');
+        controlDiv.appendChild(controlMap);
+        controlDiv.appendChild(controlSatellite);
+
+        google.maps.event.addDomListener(controlMap, 'click', function () {
+            map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
+        });
+        google.maps.event.addDomListener(controlSatellite, 'click', function () {
+            map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+        });
+    }
+
+    var zoomControlDiv = document.createElement('div');
+    var maptypeControlDiv = document.createElement('div');
+    var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+    var maptypeControl = new CustomBasemapControl(maptypeControlDiv, map);
+
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(zoomControlDiv);
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(maptypeControlDiv);
+
+    var searchPlace = new AutoComplete(map);
+
+};
+
+
+},{"./autocomplete":1,"./mapstyle":3}],3:[function(require,module,exports){
+module.exports = {
+    getStyle: function () {
+
+        var main_color = '#149DB5',
+            saturation_value = -20,
+            brightness_value = 5;
+
+        return [
+            {
+                elementType: "labels",
+                stylers: [
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "poi",
+                elementType: "labels",
+                stylers: [
+                    {visibility: "off"}
+                ]
+            },
+            {
+                //don't show highways lables on the map
+                featureType: 'road.highway',
+                elementType: 'labels',
+                stylers: [
+                    {visibility: "off"}
+                ]
+            },
+            {
+                //don't show local road lables on the map
+                featureType: "road.local",
+                elementType: "labels.icon",
+                stylers: [
+                    {visibility: "off"}
+                ]
+            },
+            {
+                featureType: "road.arterial",
+                elementType: "labels.icon",
+                stylers: [
+                    {visibility: "off"}
+                ]
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [
+                    {visibility: "off"}
+                ]
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "poi",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "poi.government",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "poi.sport_complex",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "poi.attraction",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "poi.business",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "transit.station",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "landscape",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.fill",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            },
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [
+                    {hue: main_color},
+                    {visibility: "on"},
+                    {lightness: brightness_value},
+                    {saturation: saturation_value}
+                ]
+            }
+        ];
+    }
+};
+
+},{}]},{},[2])
